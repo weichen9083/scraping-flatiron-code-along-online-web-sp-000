31723f39113762bfe84742f5e4c419a593328538
wei chen <weichen9083@gmail.com>
2020-12-20 21:36:25 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,20 +1,21 @@
 
 class Course 
   
-  attr_accessor :title, :schedule , :description
+attr_accessor :title, :schedule, :description 
+
+@@all = []
+
+
+def initialize
+  self.class.all << self
+end 
+
+def self.all 
+  @@all 
+end 
   
-  @@all = []
   
-  def initialize 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all
-  end 
-  
-  def self.reset_all 
-    @@all.clear 
-  end 
-  
+def self.reset_all
+  self.all.clear
+end 
 end 
